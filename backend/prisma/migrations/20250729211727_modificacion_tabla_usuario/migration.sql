7627a9a97af470de326ed223f9debd6c8a6de4f7
Tomi-PF <tperrino@fi.uba.ar>
2025-07-29 18:19:25 -0300

    Modificación del valor por defecto del campo autenticado de la tabla de usuario y realizado de la migración correspondiente

--- backend/prisma/migrations/20250729211727_modificacion_tabla_usuario/migration.sql
+++ backend/prisma/migrations/20250729211727_modificacion_tabla_usuario/migration.sql
@@ -4,5 +4,8 @@
   - A unique constraint covering the columns `[usuario]` on the table `Usuario` will be added. If there are existing duplicate values, this will fail.
 
 */
+-- AlterTable
+ALTER TABLE "Usuario" ALTER COLUMN "autenticado" SET DEFAULT false;
+
 -- CreateIndex
 CREATE UNIQUE INDEX "Usuario_usuario_key" ON "Usuario"("usuario");
